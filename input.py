a963402d3fced49e43c188132ba2a42fe45e93ba
Ali-anc <ali.za.ac@gmail.com>
2020-07-21 01:33:31 -0400

    changed again

---
+++ input.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+while True:
+    user=input("enter the correct word. Hint: Which OS are you on..: ")
+    if user=='linux':
+        print("you got this")
+        break
+#print(" you dont get it ")
